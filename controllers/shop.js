9b834c2366c5921ce38ad4e5eda139e35f5688db
ES\asanchezrecuero <asanchezrecuero@deloitte.es>
2024-07-10 12:11:47 +0200

    Display Cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,48 +2,60 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
-      path: "/products"
+      path: "/products",
     });
   });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId, (product) => {
     res.render("shop/product-detail", {
       product: product,
       pageTitle: product.title,
-      path: "/products"
+      path: "/products",
     });
   });
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     res.render("shop/index", {
       prods: products,
       pageTitle: "Shop",
-      path: "/"
+      path: "/",
     });
   });
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    pageTitle: "Your Cart",
-    path: "/cart"
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProduct = cart.products.find((p) => p.id === product.id);
+        if (cartProduct) {
+          cartProducts.push({ productData: product, qty: cartProduct.qty });
+        }
+      }
+      res.render("shop/cart", {
+        pageTitle: "Your Cart",
+        path: "/cart",
+        products: cartProducts,
+        totalPrice: cart.totalPrice,
+      });
+    });
   });
 };
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  Product.findById(productId, product => {
+  Product.findById(productId, (product) => {
     Cart.addProduct(productId, product.price);
-    
   });
   console.log(productId);
   res.redirect("/cart");
@@ -52,13 +64,13 @@ exports.postCart = (req, res, next) => {
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     pageTitle: "Your Orders",
-    path: "/orders"
+    path: "/orders",
   });
 };
 
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     pageTitle: "Checkout",
-    path: "/checkout"
+    path: "/checkout",
   });
 };

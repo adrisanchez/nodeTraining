b98e6c126f4effbe56675e66ea008e89f8157a33
Adrián Sánchez <asanchezrecuero@gmail.com>
2024-08-07 18:27:07 +0200

    Changes to work with Sessions

--- app.js
+++ app.js
@@ -3,25 +3,40 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://adrian:yyxalbJRsj84WQBC@cluster0.tqe0soz.mongodb.net/shop?w=majority&appName=Cluster0";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "my secret", resave: false, saveUninitialized: false, store: store })
+);
 
 app.use((req, res, next) => {
-  User.findById("66b2578dfbe300d395f2f134")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
-      // console.log("user from db " + user);
       req.user = user;
       next();
     })
@@ -30,13 +45,12 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://adrian:yyxalbJRsj84WQBC@cluster0.tqe0soz.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {

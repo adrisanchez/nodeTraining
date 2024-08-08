5914add0b6ffad33ad3e6126232c71299265e60f
Adrián Sánchez <asanchezrecuero@gmail.com>
2024-08-08 18:36:12 +0200

    Added Sendgrid integration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 const e = require("express");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        "API_KEY",
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -83,6 +94,12 @@ exports.postSignup = (req, res, next) => {
       })
       .then((result) => {
         res.redirect("/login");
+        return transporter.sendMail({
+          to: email,
+          from: "asanchezrecuero@gmail.com",
+          subject: "Signup succeeded!",
+          html: "<h1>You successfully signed up!</h1>",
+        });
       })
       .catch((err) => {
         console.log(err);

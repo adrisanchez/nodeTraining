d386de5ab90928bd3f184fb7b123e2b883632b97
ES\asanchezrecuero <asanchezrecuero@deloitte.es>
2024-08-14 15:34:54 +0200

    Async calls from client

--- controllers/admin.js
+++ controllers/admin.js
@@ -176,8 +176,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((prod) => {
       if (!prod) {
@@ -188,12 +188,11 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       console.log("DESTROYED PRODUCT");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Success!" });
     })
     .catch((err) => {
       console.log(err);
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: "Deleting product failed!" });
+
     });
 };
